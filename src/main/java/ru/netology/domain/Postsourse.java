8986dbf843aa6d9f473050b78542fabf268c7cc8
Valeriia <lera-bezborodova@mail.ru>
2020-05-09 21:19:34 +0300

    v3

---
+++ src/main/java/ru/netology/domain/Postsourse.java
@@ -0,0 +1,40 @@
+package ru.netology.domain;
+
+public class Postsourse {
+    private String type;
+    private String platform;
+    private String date;
+    private String url;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
